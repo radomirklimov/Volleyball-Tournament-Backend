a8aa1d22cc66b4269253327cb2d1a99559e4661b
Radomyr Klymov <r.klymov@jobware.de>
2026-09-22 09:07:58 +0200

    connected database

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,8 @@ plugins {
     id("org.springframework.boot") version "3.3.0"
     id("io.spring.dependency-management") version "1.1.4"
     kotlin("jvm") version "1.9.24"
-    kotlin("plugin.spring") version "1.9.0"
+    kotlin("plugin.spring") version "1.9.24"
+    kotlin("plugin.jpa") version "1.9.24"
 }
 
 group = "de.atiw.volleyball"
@@ -21,10 +22,10 @@ dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web")
 
     // JPA
-//    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 
-    // MariaDB
-//    implementation("org.mariadb.jdbc:mariadb-java-client")
+    // PostgreSQL
+    runtimeOnly("org.postgresql:postgresql")
 
     // Kotlin
     implementation("org.jetbrains.kotlin:kotlin-reflect")
